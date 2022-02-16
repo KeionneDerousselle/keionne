4ca01598222931ebc01973ccbf3203b75d8410d8
Keionne Derousselle <knnderousselle@gmail.com>
2022-02-16 15:45:39 -0700

    Add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
-export default defineNuxtConfig({})
+export default defineNuxtConfig({
+  css: ['~/assets/css/tailwind.css'],
+  build: {
+    postcss: {
+      postcssOptions: require('./postcss.config.js'),
+    },
+  },
+})
