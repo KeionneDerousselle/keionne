6ba751a9b3cb77f95cdb67081886813fd3706bc2
Keionne Derousselle <knnderousselle@gmail.com>
2022-02-18 12:28:49 -0700

    building before linting or testing so that we can extend the nuxt tsconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,48 +7,16 @@ module.exports = {
     jest: true,
     'jest/globals': true,
   },
-  parser: 'vue-eslint-parser',
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: 'module',
-    parser: '@typescript-eslint/parser',
-  },
   extends: [
-    'plugin:import/errors',
-    'plugin:import/typescript',
-    'plugin:jest/recommended',
+    '@nuxtjs/eslint-config-typescript',
     'plugin:nuxt/recommended',
-    'plugin:vue/vue3-recommended',
+    'plugin:tailwind/recommended',
     'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
   ],
-  plugins: ['@typescript-eslint', 'import', 'jest'],
+  plugins: ['jest'],
+  // add your custom rules here
   rules: {
-    '@typescript-eslint/no-shadow': ['error'],
-    '@typescript-eslint/no-unused-vars': ['error'],
-    '@typescript-eslint/no-use-before-define': ['error'],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        mjs: 'never',
-        jsx: 'never',
-        json: 'never',
-        ts: 'never',
-        tsx: 'never',
-        '': 'never',
-      },
-    ],
-    'import/no-extraneous-dependencies': 'off',
-    'import/no-unresolved': 'error',
-    'import/prefer-default-export': 'off',
-    'no-console': ['error', { allow: ['warn', 'error'] }],
-    'no-shadow': 'off',
-    'no-undef': 'off',
-    'no-unused-expressions': 'off',
-    'no-unused-vars': 'off',
-    'no-use-before-define': 'off',
     'prettier/prettier': [
       'error',
       {
@@ -61,21 +29,5 @@ module.exports = {
         ignores: ['index', 'default'],
       },
     ],
-    'vue/script-setup-no-uses-vars': 'off',
-  },
-  settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts'],
-    },
-
-    'import/resolver': {
-      node: {
-        paths: ['.'],
-        extensions: ['.js', '.mjs', '.jsx', '.ts', '.tsx', '.json'],
-      },
-      typescript: {
-        alwaysTryTypes: true,
-      },
-    },
   },
 }
